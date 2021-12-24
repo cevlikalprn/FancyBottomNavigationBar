2fa2a28aef66e1612efd17dbca9268ef243d06ec
cevlikalprn <devcalprn@gmail.com>
2021-12-24 23:26:04 +0300

    Color.kt file updated

--- BottomBar/src/main/java/com/cevlikalprn/bottombar/ui/theme/Color.kt
+++ BottomBar/src/main/java/com/cevlikalprn/bottombar/ui/theme/Color.kt
@@ -2,4 +2,7 @@ package com.cevlikalprn.bottombar.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val DefCenterItemColor = Color(0xFFFF8533)
+val CenterItemColor = Color(0xFFFF8533)
+val BottomNavBackgroundColor = Color.White
+val SelectedItemColor = Color.Blue
+val UnSelectedItemColor = Color.Black
