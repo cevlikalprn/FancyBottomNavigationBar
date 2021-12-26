b6805a2ede2e59b51f6f34424da38c7793380948
cevlikalprn <devcalprn@gmail.com>
2021-12-26 13:27:38 +0300

    Color.kt updated

--- BottomBar/src/main/java/com/cevlikalprn/bottombar/ui/theme/Color.kt
+++ BottomBar/src/main/java/com/cevlikalprn/bottombar/ui/theme/Color.kt
@@ -2,8 +2,23 @@ package com.cevlikalprn.bottombar.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val CenterItemColor = Color(0xFFFF8533)
-val BottomNavBackgroundColor = Color.White
-val SelectedItemColor = Color.Blue
-val UnSelectedItemColor = Color.Black
+//Floating Action Button Background Color
+val CenterBackgroundItemColorLight = Color(0xFFff9800)
+val CenterItemBackgroundColorDark = Color(0xFFc66900)
+
+//Navigation Bottom Bar Background Color
+val BottomNavBackgroundColorLight = Color(0XFFFFFFFF)
+val BottomNavBackgroundColorDark = Color(0xFF373737)
+
+//Unselected Nav Item
+val UnSelectedItemColorLight = Color.Black
+val UnSelectedItemColorDark = Color.White
+
+//Selected Nav Item
+val SelectedItemColor = Color(0xFFff9800)
+
+//Floating Action Button Content
 val CenterItemContentColor = Color.White
+
+//Navigation Item Background Color
+val NavItemBackgroundColor = Color.Transparent.copy(alpha = 0.05f)
